59546851cedad881c00042db1fcbb967277bc13a
withan <39221862+withab@users.noreply.github.com>
2021-04-10 22:50:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = procss.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
+  console.log(`App listening at http://host:${port}`)
 })
