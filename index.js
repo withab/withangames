25ccf61e671056a1435a46fbd4daaa798376f27f
withan <39221862+withab@users.noreply.github.com>
2021-04-10 23:06:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.use(express.static("public"))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
+app.get('/favicon.ico', (req, res) => {
+    res.sendFile(__dirname + '/favicon.ico')
+})
+
 
 app.listen(port, () => {
   console.log(`App listening at http://host:${port}`)
