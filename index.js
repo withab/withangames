21938cedda2b813e4564567b8c85f8b207cd6478
withan <39221862+withab@users.noreply.github.com>
2021-04-11 10:33:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.static("public"))
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
-app.get('/favicon.ico', (req, res) => {
-    res.sendFile(__dirname + '/favicon.ico')
-})
-
 
 app.listen(port, () => {
   console.log(`App listening at http://host:${port}`)
